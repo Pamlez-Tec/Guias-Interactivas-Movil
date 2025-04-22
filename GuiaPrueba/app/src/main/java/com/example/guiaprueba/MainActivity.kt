a15105ba76917d106242e24dcce0bfbd7b9e3ff9
Manollito <117801254+Manollito@users.noreply.github.com>
2025-04-21 20:01:58 -0600

    Se agrega la guía del signo de pregunta

--- GuiaPrueba/app/src/main/java/com/example/guiaprueba/MainActivity.kt
+++ GuiaPrueba/app/src/main/java/com/example/guiaprueba/MainActivity.kt
@@ -13,6 +13,8 @@
     import android.view.ViewGroup
     import android.widget.ImageButton
     import android.widget.Toast
+    import android.widget.TextView
+
 
     class MainActivity : AppCompatActivity() {
 
@@ -45,6 +47,64 @@
             TabLayoutMediator(tabLayout, viewPager) { tab, position ->
                 tab.text = tabTitles[position]
             }.attach()
+
+
+            val guideOverlay = findViewById<View>(R.id.guideOverlay)
+            val guideText = findViewById<TextView>(R.id.guideText)
+            val btnNext = findViewById<ImageButton>(R.id.btnNext)
+            val btnBack = findViewById<ImageButton>(R.id.btnBack)
+
+            val guideSteps = listOf(
+                "Bienvenido a la Guía de aplicaciones. Esperamos que su aprendizaje resulte agradable.\n\n" +
+                        "Estos mensajes le guiarán paso a paso, en este caso le enseñará a utilizar esta aplicación. " +
+                        "Para continuar con el tutorial presione la flecha que se encuentra en la parte inferior derecha.",
+
+                "En caso de que haya presionado sin querer el botón para avanzar el tutorial, puede volver al mensaje anterior " +
+                        "al presionar la flecha que está en la parte inferior izquierda.",
+
+                "Los tutoriales solamente aparecerán una vez, esto le permetirá probar libremente las funciones que simula la app, " +
+                        "si desea volver a ver el tutorial de la sección en la que se encuentra, presione el botón señalado.",
+
+                "Organizamos los tutoriales en categorías que le ayudarán a encontrar más fácilmente el que desea realizar, para " +
+                        "elegir una categoría debe presionar el nombre que se encuentra en la barra de categorías. \n" +
+                        "Hay ocasiones en las que debe realizar una acción para avanzar en el tutorial, en este caso seleccione " +
+                        "la categoría Redes Sociales.",
+
+                "¡Felicidades!\n" +
+                        "Ya sabes cómo utilizar la applicación. Ahora puedes elegir cualquier tutorial de las aplicaciones que desee."
+            )
+            var currentStep = 0
+
+            fun updateGuideStep() {
+                guideText.text = guideSteps[currentStep]
+                btnBack.visibility = if (currentStep == 0) View.GONE else View.VISIBLE
+                btnNext.visibility = if (currentStep == guideSteps.size - 1) View.GONE else View.VISIBLE
+            }
+
+            btnHelp.setOnClickListener {
+                guideOverlay.visibility = View.VISIBLE
+                currentStep = 0
+                updateGuideStep()
+            }
+
+            btnNext.setOnClickListener {
+                if (currentStep < guideSteps.size - 1) {
+                    currentStep++
+                    updateGuideStep()
+                }
+            }
+
+            btnBack.setOnClickListener {
+                if (currentStep > 0) {
+                    currentStep--
+                    updateGuideStep()
+                }
+            }
+
+            guideOverlay.setOnClickListener {
+                guideOverlay.visibility = View.GONE
+            }
+
         }
     }
 
